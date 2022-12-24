6461e3703c4e13c62bb871fe4646e64810e108bd
petraelgemyr <petra.elgemyr@gmail.com>
2022-12-24 01:07:09 +0100

    added if-else to empty list and payment button

--- src/ts/shoppingCart.ts
+++ src/ts/shoppingCart.ts
@@ -1,10 +1,10 @@
 import { Product } from "./models/Products";
 let rootDiv: HTMLDivElement = document.getElementById("root") as HTMLDivElement;
-window.onload = function () {
-  let cartProducts: Product[] = JSON.parse(
-    localStorage.getItem("storageList") || "[]"
-  );
+let cartProducts: Product[] = JSON.parse(
+  localStorage.getItem("storageList") || "[]"
+);
 
+window.onload = function () {
   startUp();
   showShoppingCart(cartProducts);
   console.log(cartProducts);
@@ -21,20 +21,31 @@ function emptyShoppingCart(cartProducts: Product[]) {
   localStorage.setItem("storageList", JSON.stringify(cartProducts));
   console.log(cartProducts);
   rootDiv.innerHTML = "";
+  showShoppingCart(cartProducts);
 }
 
 function startUp() {
   let orderContainer: HTMLParagraphElement = document.getElementById(
     "container__ordernumber"
   ) as HTMLParagraphElement;
+  //behövs detta?? ^^
 
   let pay: HTMLButtonElement = document.getElementById(
     "pay"
   ) as HTMLButtonElement;
 
   pay.addEventListener("click", () => {
-    window.location.href = "../pages/orderConfirmation.html";
+    handlePayClick();
   });
+}
+
+function handlePayClick() {
+  if (cartProducts.length !== 0) {
+    window.location.href = "../pages/orderConfirmation.html";
+    emptyShoppingCart(cartProducts);
+  } else {
+    alert("Ojdå! Ser ut som att din varukorg är tom!");
+  }
 }
 
 // pay.addEventListener("click", () => {
@@ -57,31 +68,37 @@ function startUp() {
 // console.log(pay);
 
 const showShoppingCart = (cartProducts: Product[]) => {
-  for (let i = 0; i < cartProducts.length; i++) {
-    let cartArticle: HTMLDivElement = document.createElement("div");
-    cartArticle.className = "article--container";
+  if (cartProducts.length === 0) {
+    let emptyText: HTMLHeadingElement = document.createElement("h3");
+    emptyText.innerHTML = "Din varukorg är tom!";
+    rootDiv.appendChild(emptyText);
+  } else {
+    for (let i = 0; i < cartProducts.length; i++) {
+      let cartArticle: HTMLDivElement = document.createElement("div");
+      cartArticle.className = "article--container";
 
-    let articleImg: HTMLImageElement = document.createElement("img");
-    articleImg.src = cartProducts[i].imageUrl;
-    articleImg.alt = cartProducts[i].name;
-    articleImg.className = "article--pic";
+      let articleImg: HTMLImageElement = document.createElement("img");
+      articleImg.src = cartProducts[i].imageUrl;
+      articleImg.alt = cartProducts[i].name;
+      articleImg.className = "article--pic";
 
-    let articleTitle: HTMLParagraphElement = document.createElement("p");
-    articleTitle.innerHTML = cartProducts[i].name;
+      let articleTitle: HTMLParagraphElement = document.createElement("p");
+      articleTitle.innerHTML = cartProducts[i].name;
 
-    let articleAmount: HTMLParagraphElement = document.createElement("p");
-    let amountText = cartProducts[i].amount.toString();
-    articleAmount.innerHTML = amountText + " st";
+      let articleAmount: HTMLParagraphElement = document.createElement("p");
+      let amountText = cartProducts[i].amount.toString();
+      articleAmount.innerHTML = amountText + " st";
 
-    let articlePrice: HTMLParagraphElement = document.createElement("p");
-    let priceText = cartProducts[i].price.toString();
-    articlePrice.innerHTML = priceText + " kr";
+      let articlePrice: HTMLParagraphElement = document.createElement("p");
+      let priceText = cartProducts[i].price.toString();
+      articlePrice.innerHTML = priceText + " kr";
 
-    cartArticle.appendChild(articleImg);
-    cartArticle.appendChild(articleTitle);
-    cartArticle.appendChild(articleAmount);
-    cartArticle.appendChild(articlePrice);
+      cartArticle.appendChild(articleImg);
+      cartArticle.appendChild(articleTitle);
+      cartArticle.appendChild(articleAmount);
+      cartArticle.appendChild(articlePrice);
 
-    rootDiv.appendChild(cartArticle);
+      rootDiv.appendChild(cartArticle);
+    }
   }
 };
